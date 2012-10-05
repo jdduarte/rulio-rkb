c26b5f21e0d49b88f826620116026cc65d72531b
jdduarte <jdduarte@gmail.com>
2012-10-05 23:30:18 +0100

    Initial commit

---
+++ rkb.js
@@ -0,0 +1,27 @@
+module.exports = exports = function(client) {
+  var fs = require('fs');
+  var conf = JSON.parse(fs.readFileSync(__dirname + '/rkb.config.json').toString());
+
+  client.addListener('message', function (nick, to, text, message) {
+     var match = getSomethingToSay(text, conf);
+     if(match){
+     	client.say(to, match);
+     }
+  });
+};
+
+function getSomethingToSay(text, conf){
+  for(var e in conf){
+    if(text.toLowerCase().indexOf(e) != -1){
+      var index = randomFromInterval(0, conf[e].length - 1);
+      return conf[e][index];
+    }
+  }
+};
+
+function randomFromInterval(from,to)
+{
+    return Math.floor(Math.random()*(to-from+1)+from);
+};
+
+require('./app.js');
