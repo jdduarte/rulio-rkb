b6d387b7bb77478b43c579b304cc73667aeeea0a
Fabio Oliveira <fabio.an.oliveira@gmail.com>
2012-10-11 23:40:14 +0100

    Added RegExp support to rkb.js, along with memoization to avoid the creation of too many RegExp objects.

--- rkb.js
+++ rkb.js
@@ -3,18 +3,22 @@ module.exports = exports = function(client) {
   var fs = require('fs');
   //var conf = JSON.parse(fs.readFileSync(__dirname + '/rkb.config.json').toString());
   conf = liveconf(__dirname + '/rkb.config.json');
+  precompiledRegexs = {};
 
   client.addListener('message', function (nick, to, text, message) {
      var match = getSomethingToSay(text, conf);
-     if(match){
-     	client.say(to, match);
+     if(match) {
+      client.say(to, match);
      }
   });
 };
 
-function getSomethingToSay(text, conf){
-  for(var e in conf){
-    if(text.toLowerCase().indexOf(e) != -1){
+function getSomethingToSay(text, conf) {
+  for(var e in conf) {
+    if(!precompiledRegexs[e])
+      precompiledRegexs[e] = new RegExp(e);
+    
+    if(precompiledRegexs[e].test(text)) {
       var index = randomFromInterval(0, conf[e].length - 1);
       return conf[e][index];
     }
