55fdb0f042020954e8a2861223f1929cfeb1fdab
fcovas <francisco.c.covas@gmail.com>
2012-10-17 20:17:50 +0100

    added normalization logic

--- rkb.js
+++ rkb.js
@@ -3,6 +3,17 @@ module.exports = exports = function(client) {
   var fs = require('fs');
   //var conf = JSON.parse(fs.readFileSync(__dirname + '/rkb.config.json').toString());
   conf = liveconf(__dirname + '/rkb.config.json');
+  conf.ee.on('changed', function(){
+    for(var e in conf){
+      var ceiling;
+      if(!e.prob){ 
+        e.prob = 1;
+      }
+      ceiling = e.prob;
+
+      normalizeConf(e);
+    }
+  });
 
   client.addListener('message', function (nick, to, text, message) {
      var match = getSomethingToSay(text, conf);
@@ -34,4 +45,33 @@ function randomFromInterval(from, to)
     return Math.floor(Math.random()*(to-from+1)+from);
 };
 
+function normalizeConf(conf){
+  var sumOfProbs = 0; var sumOfLackingProb = 0; var lackingProbProb = 0;
+
+  for(var o in e.options){
+    if(o.prob){
+      sumOfProbs += o.prob;
+    } else {
+      ++sumOfLackingProb;
+    }
+  }
+
+  if(sumOfProbs < 1){
+    lackingProbProb = (1 - sumOfProbs)/sumOfLackingProb;
+
+    for(var o in e.options){
+      if(!o.prob){
+        o.prob = lackingProbProb;
+      }
+    }  
+  } else {
+    for(var o in e.options){
+      if(!o.prob){
+        o.prob = 0;
+      } else {
+        o.prob = o.prob / sumOfProbs;
+      }
+    }  
+  }
+}
 require('./app.js');
